59a7393726f3ad12c732dea5e31248b117bcfeb3
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-13 02:46:49 +0300

    add postgres config

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,43 @@
+package postgres
+
+import (
+	"avito-internship/pkg/e"
+	"avito-internship/pkg/logger"
+	"context"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PgDatabase struct {
+	Pool *pgxpool.Pool
+}
+
+func NewPgDatabase(pool *pgxpool.Pool) *PgDatabase {
+	return &PgDatabase{Pool: pool}
+}
+
+func Connect(logger logger.Logger) (*PgDatabase, error) {
+	const op = "PgDatabase.Connect"
+
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, e.Wrap(op, err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, e.Wrap(op, err)
+	}
+
+	return NewPgDatabase(pool), nil
+}
+
+func (db *PgDatabase) Close() {
+	if db.Pool != nil {
+		db.Pool.Close()
+	}
+}
