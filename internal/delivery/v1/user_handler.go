ade7b1a77e33ac6b04a7ca680f10f9147ff3374d
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-13 19:49:15 +0300

    implement user_handler.go

---
+++ internal/delivery/v1/user_handler.go
@@ -0,0 +1,40 @@
+package v1
+
+import (
+	"avito-internship/pkg/e"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) setIsActive(c *gin.Context) {
+	var req SetIsActiveReq
+	if err := c.ShouldBind(&req); err != nil {
+		c.Error(e.ErrInvalidRequestBody)
+		return
+	}
+
+	res, err := h.SetIsActive(c.Request.Context(), toUseCaseSetIsActiveReq(req))
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, toDeliverySetIsActiveRes(res))
+}
+
+func (h *Handler) getReview(c *gin.Context) {
+	var req GetReviewQueryReq
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.Error(e.ErrInvalidRequestBody)
+		return
+	}
+
+	res, err := h.GetReview(c.Request.Context(), req.UserID)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, toDeliveryGetReviewRes(res))
+}
