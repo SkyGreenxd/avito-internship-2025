86e2cc55462da4a223119e5a3aaf6073c4f02a22
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-13 20:03:11 +0300

    change getTeam method

--- internal/delivery/v1/team_handler.go
+++ internal/delivery/v1/team_handler.go
@@ -24,7 +24,13 @@ func (h *Handler) addTeam(c *gin.Context) {
 }
 
 func (h *Handler) getTeam(c *gin.Context) {
-	res, err := h.GetTeam(c.Request.Context())
+	var req GetTeamQueryReq
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.Error(e.ErrInvalidRequestBody)
+		return
+	}
+
+	res, err := h.GetTeam(c.Request.Context(), req.TeamName)
 	if err != nil {
 		c.Error(err)
 		return
