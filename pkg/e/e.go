bca09ed4abafe58ea51694ade8bbd2919c69ee6f
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-13 19:46:43 +0300

    add new errors

--- pkg/e/e.go
+++ pkg/e/e.go
@@ -3,12 +3,37 @@ package e
 import "fmt"
 
 var (
-	ErrTeamIsExists = fmt.Errorf("team with this name already exists")
+	// Ошибки с командами
+	ErrTeamIsExists = fmt.Errorf("team_name already exists")
 	ErrTeamNotFound = fmt.Errorf("team not found")
 
+	// Ошибки с пользователями
 	ErrUserNotFound = fmt.Errorf("user not found")
 
-	PRIsExists = fmt.Errorf("pool request is exists")
+	// Ошибки с PullRequest
+	ErrPRIsExists            = fmt.Errorf("PR id already exists")
+	ErrPRNotFound            = fmt.Errorf("pool request not found")
+	ErrPrMerged              = fmt.Errorf("cannot reassign on merged PR")
+	ErrPrReviewerNotAssigned = fmt.Errorf("reviewer is not assigned to this PR")
+	ErrPrNoCandidate         = fmt.Errorf("no active replacement candidate in team")
+
+	// Ошибки пользователя
+	ErrInvalidRequestBody = fmt.Errorf("invalid request body")
+	ErrResourceNotFound   = fmt.Errorf("resource not found")
+	ErrUnauthorized       = fmt.Errorf("unauthorized")
+
+	// Ошибки сервера
+	ErrInternalServerError = fmt.Errorf("internal server error")
+)
+
+const (
+	NOT_FOUND    = "NOT_FOUND"
+	TEAM_EXISTS  = "TEAM_EXISTS"
+	PR_EXISTS    = "PR_EXISTS"
+	PR_MERGED    = "PR_MERGED"
+	NOT_ASSIGNED = "NOT_ASSIGNED" // TODO: ошибка возникает, когда пытаются переназначить кого-то, кто не назначен на этот PR.
+	NO_CANDIDATE = "NO_CANDIDATE" // TODO: когда нет активных пользователей в команде для замены ревьювера
+	SERVER_ERR   = "SERVER_ERR"
 )
 
 func Wrap(msg string, err error) error {
