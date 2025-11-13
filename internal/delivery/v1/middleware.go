ffa1126893a8538744ee33695ea1495d6a9cf580
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-13 19:48:11 +0300

    implement AdminMiddleware and ErrorMiddleware

---
+++ internal/delivery/v1/middleware.go
@@ -0,0 +1,68 @@
+package v1
+
+import (
+	"avito-internship/pkg/e"
+	"avito-internship/pkg/logger"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Middleware struct {
+	logger     logger.Logger
+	adminToken string
+}
+
+func NewMiddleware(logger logger.Logger, adminToken string) *Middleware {
+	return &Middleware{
+		logger:     logger,
+		adminToken: adminToken,
+	}
+}
+
+func (m *Middleware) AdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const prefix = "Bearer "
+		authHeader := c.GetHeader("Authorization")
+		token := strings.TrimPrefix(authHeader, prefix)
+
+		if !strings.HasPrefix(authHeader, prefix) || token != m.adminToken {
+			m.logger.Error(e.ErrUnauthorized, "",
+				"method", c.Request.Method,
+				"path", c.Request.URL.Path,
+			)
+
+			abortUnauthorized(c)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func (m *Middleware) ErrorMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		for _, ginErr := range c.Errors {
+			err := ginErr.Err
+
+			m.logger.Error(err, "",
+				"method", c.Request.Method,
+				"path", c.Request.URL.Path,
+			)
+
+			codeInt, codeString, msg := ToHTTPResponse(err)
+			response := NewErrorResponse(codeString, msg)
+			c.JSON(codeInt, response)
+			return
+		}
+	}
+}
+
+func abortUnauthorized(c *gin.Context) {
+	response := NewErrorResponse(e.NOT_FOUND, e.ErrResourceNotFound.Error())
+	c.JSON(http.StatusUnauthorized, response)
+	c.Abort()
+}
