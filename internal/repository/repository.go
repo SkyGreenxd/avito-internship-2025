7aeb09c019183a670503c576174dcaaeebbf736e
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-15 00:38:15 +0300

    add status repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -13,7 +13,7 @@ type UserRepository interface {
 }
 
 type TeamRepository interface {
-	Create(ctx context.Context, team domain.Team) (domain.Team, error) // TODO создает только команду
+	Create(ctx context.Context, team domain.Team) (domain.Team, error)
 	GetMembersByTeamNameWithUsers(ctx context.Context, teamName string) ([]domain.User, error)
 	GetTeamByUserId(ctx context.Context, userId string) (domain.Team, error)
 	AddUsersToTeam(ctx context.Context, teamId int, users []domain.User) ([]domain.User, error)
@@ -21,12 +21,17 @@ type TeamRepository interface {
 
 type PullRequestRepository interface {
 	Create(ctx context.Context, pullRequest domain.PullRequest) (domain.PullRequest, error)
-	SetMergedStatus(ctx context.Context, prId string) (domain.PullRequest, []string, error)
-	GetByPrIdWithReviewersIds(ctx context.Context, prId string) (domain.PullRequest, []string, error)
+	SetMergedStatus(ctx context.Context, statusId int, prId string) (SetMergedStatusDTO, error)
+	GetByPrIdWithReviewersIds(ctx context.Context, prId string) (GetByPrIdWithReviewersIdsDTO, error)
 }
 
 type PrReviewerRepository interface {
 	AddReviewers(ctx context.Context, pullRequestId string, reviewersId []string) error
-	GetPRByReviewer(ctx context.Context, userId string) ([]domain.PullRequest, error)
+	GetPRByReviewer(ctx context.Context, userId string) (GetPRByReviewerDTO, error)
 	UpdateReviewer(ctx context.Context, oldUserId string, newUserId string, pullRequestId string) (string, error)
+}
+
+type StatusRepository interface {
+	GetById(ctx context.Context, statusId int) (domain.Status, error)
+	GetByName(ctx context.Context, statusName string) (domain.Status, error)
 }
