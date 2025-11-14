edce762da3847f0e9343d3ee9502a3f42c033c32
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-15 00:38:50 +0300

    change status to status id

--- internal/domain/pull_request.go
+++ internal/domain/pull_request.go
@@ -2,29 +2,22 @@ package domain
 
 import "time"
 
-type PRStatus string
-
-const (
-	OPEN   PRStatus = "OPEN"
-	MERGED PRStatus = "MERGED"
-)
-
 type PullRequest struct {
 	Id                string
 	Name              string
 	AuthorId          string
-	Status            PRStatus
+	StatusId          int
 	NeedMoreReviewers bool
 	CreatedAt         time.Time
 	MergedAt          *time.Time
 }
 
-func NewPoolRequest(id, name, authorId string) *PullRequest {
+func NewPoolRequest(id, name, authorId string, statusId int) *PullRequest {
 	return &PullRequest{
 		Id:                id,
 		Name:              name,
 		AuthorId:          authorId,
-		Status:            OPEN,
+		StatusId:          statusId,
 		NeedMoreReviewers: true,
 		CreatedAt:         time.Now(),
 	}
