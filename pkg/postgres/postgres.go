d25bedd748b669f573c86bc7071260cc9c58fddf
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-15 17:52:08 +0300

    add ssl mode

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -29,12 +29,13 @@ func NewPgDatabase(pool *pgxpool.Pool, dsn string) *PgDatabase {
 func Connect() (*PgDatabase, error) {
 	const op = "PgDatabase.Connect"
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
+		os.Getenv("SSL_MODE"),
 	)
 
 	pool, err := pgxpool.New(context.Background(), dsn)
