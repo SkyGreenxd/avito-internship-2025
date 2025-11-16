9016466a8943df5463be6aafb374b7b7701b2d7d
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-11-16 06:12:59 +0300

    add new endpoint

--- internal/delivery/v1/team_handler.go
+++ internal/delivery/v1/team_handler.go
@@ -38,3 +38,19 @@ func (h *Handler) getTeam(c *gin.Context) {
 
 	c.JSON(http.StatusOK, toDeliveryGetTeamRes(res))
 }
+
+func (h *Handler) deactivateMembers(c *gin.Context) {
+	var req DeactivateMembersReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.Error(e.Wrap(err.Error(), e.ErrInvalidRequestBody))
+		return
+	}
+
+	res, err := h.teamUC.DeactivateMembers(c.Request.Context(), toUseCaseDeactivateMembers(req))
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, toDeliveryDeactivateMembers(res))
+}
